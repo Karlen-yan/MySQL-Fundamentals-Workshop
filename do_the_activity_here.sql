760e40b6824432beaee1ca6a5928434a328d2182
Karlen/Hakobyan <102352076+Karlen-yan@users.noreply.github.com>
2023-08-07 18:24:52 +0200

    Create do_the_activity_here.sql

---
+++ do_the_activity_here.sql
@@ -0,0 +1,28 @@
+-- Actividad 1: Crear una Tabla
+
+-- Crear una nueva base de datos
+
+
+-- Usar la base de datos creada
+
+
+-- Crear la tabla de usuarios
+
+
+-- Actividad 2: Insertar Datos
+
+-- Insertar registros en la tabla usuarios
+
+
+-- Actividad 3: Consultas Básicas
+
+-- Seleccionar todos los registros de la tabla usuarios
+
+-- Seleccionar el nombre y correo de usuarios con id mayor a 1
+
+
+-- Actividad 4: Actualizar Datos
+
+-- Actualizar el correo de un usuario específico
+
+-- Actividad 5: Borrar Datos
